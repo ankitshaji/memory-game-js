2dc0346ededb2c2436943af013ed10b63d7a6037
Ankit Shaji <ankitshaji@gmail.com>
2022-06-02 00:23:34 +1200

    flipCard function

--- src/app.js
+++ src/app.js
@@ -51,21 +51,35 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
-  cards.sort(() => 0.5 -Math.random());
-  const grid = document.querySelector('.grid');
-  function createBoard(){
-      for(let i=0;i<cards.length;i++){
-          const card = document.createElement('img')
-          card.setAttribute('src','src/images/blank.png')
-          card.setAttribute('width','100px')
-          card.setAttribute('height','100px')
-          card.setAttribute('data-id',i)
-          card.addEventListener('click',flipCard)
-          grid.appendChild(card)
-      }
+  cards.sort(() => 0.5 - Math.random());
+  const grid = document.querySelector(".grid");
+  let cardsChosen = [];
+  let cardsChosenIds = [];
+  function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "src/images/blank.png");
+      card.setAttribute("width", "100px");
+      card.setAttribute("height", "100px");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
   }
-  createBoard();
-  function flipCard(){
+  
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cards[cardId].name);
+    cardsChosenIds.push(cardId);
+    this.setAttribute('src',cards[cardId].img)
+    if(cardsChosen.length == 2){
+        setTimeout(checkForMatch,500)
+    }
+  }
+
+  function checkForMatch(){
       
   }
+
+  createBoard();
 });
